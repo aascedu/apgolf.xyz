152c787502bc0bf9b4215860413406bf76b205db
Arthur Ascedu <aascedu@z4r3p1.42lyon.fr>
2025-06-06 10:37:17 +0200

    feat(Inception): added source files for my app

---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,35 @@
+#!bin/bash
+
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+
+wp core download --path=/var/www/html \
+                 --allow-root
+
+wp config create	--allow-root \
+                    --dbname=$SQL_DATABASE \
+                    --dbuser=$SQL_ADMIN \
+                    --dbpass=$SQL_ADMINPASSWD \
+                    --dbhost=mariadb:3306 \
+                    --path=/var/www/html
+
+wp core install     --allow-root \
+                    --url=$DOMAIN_NAME \
+                    --admin_user=$SQL_ADMIN \
+                    --admin_password=$SQL_ADMINPASSWD \
+                    --admin_email=aascedu@student.42lyon.fr \
+                    --title=Inception \
+                    --path=/var/www/html
+
+wp user create      --allow-root \
+                    $SQL_USER \
+                    example@example.com \
+                    --user_pass=$SQL_USERPASSWD \
+                    --path=/var/www/html
+
+fi
+
+if [ ! -d "/run/php" ]; then
+    mkdir -p /run/php
+fi
+
+/usr/sbin/php-fpm7.4 -F
