7c9cd097b7e5245594adb038bcd710d79ddd89d6
Arthur Ascedu <aascedu@z4r8p6.42lyon.fr>
2025-06-26 14:48:17 +0200

    feat(component): wip le coach component

---
+++ server/VueJS/src/components/CoachHome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full px-8 mt-[2vw] mb-[2vw]">
+    <!-- Title with Horizontal Line -->
+    <div class="flex items-center">
+      <div class="flex-1 max-w-[17vw] h-[3px] bg-[#ffc107] ml-[2vw] mr-[2vw]"></div>
+      <!-- Title -->
+      <h2 class="text-[2vw] font-bold uppercase text-green-700 whitespace-nowrap">
+        Le Coach
+      </h2>
+      <!-- Horizontal Line -->
+      <div class="flex-1 h-[3px] bg-[#ffc107] ml-[2vw] mr-[2vw]"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
