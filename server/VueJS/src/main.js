c0aaaccda13c9f472300c48c2a3bd692f6ba6463
Arthur Ascedu <aascedu@z4r8p6.42lyon.fr>
2025-06-30 14:03:40 +0200

    feat(view): init new view

--- server/VueJS/src/main.js
+++ server/VueJS/src/main.js
@@ -6,6 +6,7 @@ import Home from './views/Home.vue'
 import Contact from './views/Contact.vue'
 import Stages from './views/Stages.vue'
 import Propos from './views/Propos.vue'
+import Stage1 from './views/Stage1.vue'
 
 import './css/main.css'
 
@@ -14,6 +15,7 @@ const routes = [
   { path: '/stages', component: Stages },
   { path: '/contact', component: Contact },
   { path: '/propos', component: Propos },
+  { path: '/stage1', component: Stage1 },
 ]
 
 const router = createRouter({
---
+++ server/VueJS/src/views/Stage1.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="relative mt-[64px] md:mt-0">
+        <h1>
+        Stage1
+    </h1>
+    <h1>
+        Stage1
+    </h1>
+    <h1>
+        Stage1
+    </h1>
+    <h1>
+        Stage1
+    </h1>
+    Work in progress
+    </div>
+</template>
+
+<script>
+
+export default {
+    components: {
+    },
+};
+</script>
