783d0bf7be095c5d237b905f561577c73187c51c
Arthur Ascedu <aascedu@z4r8p6.42lyon.fr>
2025-07-01 12:59:17 +0200

    feat(propos): a propos page is done

--- server/VueJS/src/views/Propos.vue
+++ server/VueJS/src/views/Propos.vue
@@ -1,25 +1,14 @@
 <template>
-    <div class="relative mt-[64px] md:mt-0">
-        <h1>
-        Presentation Pierre ASCEDU
-    </h1>
-    <h1>
-        Presentation Pierre ASCEDU
-    </h1>
-    <h1>
-        Presentation Pierre ASCEDU
-    </h1>
-    <h1>
-        Presentation Pierre ASCEDU
-    </h1>
-    Work in progress
-    </div>
+    <Presentation />
 </template>
 
 <script>
 
+import Presentation from '@/components/Presentation.vue';
+
 export default {
     components: {
+        Presentation,
     },
 };
 </script>
