b5fcef97e01130913ffa404a349255bbb85e9073
Arthur Ascedu <aascedu@z3r8p2.42lyon.fr>
2025-09-09 15:12:47 +0200

    feat: proam pic

--- server/VueJS/src/views/Home.vue
+++ server/VueJS/src/views/Home.vue
@@ -21,7 +21,7 @@
           href="/sejours"
         />
         <HomeElement 
-          image="/assets/Accueil-ProAm.jpg"
+          image="/assets/ProAm.jpg"
           :title="$t('home.competitions.title')"
           :description="$t('home.competitions.description')"
           :buttonText="$t('home.competitions.buttonText')"
