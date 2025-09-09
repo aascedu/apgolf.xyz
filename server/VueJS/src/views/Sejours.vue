6081a688c1fb61734bdc2de5ae053bd040d8f2b1
Arthur Ascedu <aascedu@z3r8p2.42lyon.fr>
2025-09-09 10:05:53 +0200

    feat: change server conf and add photos

--- server/VueJS/src/views/Sejours.vue
+++ server/VueJS/src/views/Sejours.vue
@@ -31,24 +31,6 @@
                     description="21-23 juin 2025"
                     link="/test-sejour"
                 />
-                <ItemElement 
-                    image="/assets/home_banner.jpeg"
-                    title="Golf à Chamonix - Initiation"
-                    description="5-7 juillet 2025"
-                    link="/test-sejour"
-                />
-                <ItemElement 
-                    image="/assets/home_banner.jpeg"
-                    title="Golf à Évian - Junior"
-                    description="12-14 juillet 2025"
-                    link="/test-sejour"
-                />
-                <ItemElement 
-                    image="/assets/home_banner.jpeg"
-                    title="Golf à Megève - Famille"
-                    description="19-21 juillet 2025"
-                    link="/test-sejour"
-                />
         </div>
         <StageCustom 
             image="/assets/home_banner.jpeg"
--- server/VueJS/src/views/Stages.vue
+++ server/VueJS/src/views/Stages.vue
@@ -24,36 +24,6 @@
                 description="7-9 juin 2025"
                 link="/stage-entreprise"
             />
-            <ItemElement 
-                image="/assets/home_banner.jpeg"
-                title="Golf à Paris"
-                description="21-23 juin 2025"
-                link="/stage-intensif"
-            />
-            <ItemElement 
-                image="/assets/home_banner.jpeg"
-                title="Golf à Chamonix - Initiation"
-                description="5-7 juillet 2025"
-                link="/stage-swing-house"
-            />
-            <ItemElement 
-                image="/assets/home_banner.jpeg"
-                title="Golf à Évian - Junior"
-                description="12-14 juillet 2025"
-                link="/stage-entreprise"
-            />
-            <ItemElement 
-                image="/assets/home_banner.jpeg"
-                title="Golf à Megève - Famille"
-                description="19-21 juillet 2025"
-                link="/stage-intensif"
-            />
-            <ItemElement 
-                image="/assets/home_banner.jpeg"
-                title="Golf à Paris - Compétition"
-                description="26-28 juillet 2025"
-                link="/stage-swing-house"
-            />
         </div>
         <StageCustom 
             image="/assets/home_banner.jpeg"
